812745294ec40857bc79418bbea2a011ca792dcf
Jake <schoonbrood.jake@gmail.com>
2021-09-18 16:26:56 +0100

    UI Redesign

--- packages/engines/checklist.py
+++ packages/engines/checklist.py
@@ -20,7 +20,7 @@ class Widget(QtWidgets.QWidget):
         self.populate_menu()
 
     def constrct_ui(self):
-        self.vertical_spacer = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.spacer = QtWidgets.QSpacerItem(1, 1, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
 
         self.grid_layout = QtWidgets.QGridLayout()
         self.grid_layout.setSizeConstraint(QtWidgets.QLayout.SizeConstraint.SetMaximumSize)
@@ -35,139 +35,78 @@ class Widget(QtWidgets.QWidget):
 
         # Engine Block
 
-        self.block_label = QtWidgets.QLabel("Engine Block")
-        self.block_label.setFont(title_font)
-        self.block_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.grid_layout.addWidget(self.block_label, 1, 0, 1, 1)
-
         self.block_widget = QtWidgets.QWidget()
         self.block_layout = QtWidgets.QGridLayout()
         self.block_layout.setColumnMinimumWidth(0, 1)
 
-        self.procedures = QtWidgets.QLabel("Procedures")
-        self.procedures.setAlignment(QtCore.Qt.AlignLeft)
-        self.procedures.setFont(main_font)
-        self.block_layout.addWidget(self.procedures, 0, 0, 1, 2)
-
-        self.time = QtWidgets.QLabel("Time")
-        self.time.setFont(main_font)
-        self.block_layout.addWidget(self.time, 0, 3, 1, 1)
-
-        self.cost = QtWidgets.QLabel("Cost")
-        self.cost.setFont(main_font)
-        self.cost.setAlignment(QtCore.Qt.AlignCenter)
-        self.block_layout.addWidget(self.cost, 0, 4, 1, 1)
-
-        self.block_checkbox = QtWidgets.QCheckBox()
-        self.block_layout.addWidget(self.block_checkbox, 1, 0, 1, 1)
         self.block_clearances = QtWidgets.QPushButton("Block")
         self.block_clearances.setObjectName("Block")
         self.block_clearances.clicked.connect(lambda: self.widget_change(self.block_clearances))
         self.block_clearances.setMaximumSize(self.block_clearances.sizeHint())
         self.block_clearances.setFont(main_font)
-        self.block_layout.addWidget(self.block_clearances, 1, 1, 1, 1)
+        self.block_layout.addWidget(self.block_clearances, 1, 0, 1, 1)
 
-        self.piston_checkbox = QtWidgets.QCheckBox()
-        self.block_layout.addWidget(self.piston_checkbox, 2, 0, 1, 1)
         self.piston_label = QtWidgets.QLabel("Pistons")
         self.piston_label.setFont(main_font)
-        self.block_layout.addWidget(self.piston_label, 2, 1, 1, 1)
+        self.block_layout.addWidget(self.piston_label, 2, 0, 1, 1)
 
-        self.conrods_checkbox = QtWidgets.QCheckBox()
-        self.block_layout.addWidget(self.conrods_checkbox, 3, 0, 1, 1)
         self.conrods_label = QtWidgets.QLabel("ConRods")
         self.conrods_label.setFont(main_font)
-        self.block_layout.addWidget(self.conrods_label, 3, 1, 1, 1)
+        self.block_layout.addWidget(self.conrods_label, 3, 0, 1, 1)
 
-        self.balancing_checkbox = QtWidgets.QCheckBox()
-        self.block_layout.addWidget(self.balancing_checkbox, 4, 0, 1, 1)
         self.balancing_label = QtWidgets.QLabel("Balancing")
         self.balancing_label.setFont(main_font)
-        self.block_layout.addWidget(self.balancing_label, 4, 1, 1, 1)
+        self.block_layout.addWidget(self.balancing_label, 4, 0, 1, 1)
 
-        self.crank_checkbox = QtWidgets.QCheckBox()
-        self.block_layout.addWidget(self.crank_checkbox, 5, 0, 1, 1)
         self.crank_label = QtWidgets.QLabel("Crank & Bearings")
         self.crank_label.setFont(main_font)
-        self.block_layout.addWidget(self.crank_label, 5, 1, 1, 1)
+        self.block_layout.addWidget(self.crank_label, 5, 0, 1, 1)
 
-        self.oilpump_checkbox = QtWidgets.QCheckBox()
-        self.block_layout.addWidget(self.oilpump_checkbox, 6, 0, 1, 1)
         self.oilpump_label = QtWidgets.QLabel("Oil Pump")
         self.oilpump_label.setFont(main_font)
-        self.block_layout.addWidget(self.oilpump_label, 6, 1, 1, 1)
+        self.block_layout.addWidget(self.oilpump_label, 6, 0, 1, 1)
 
-        self.parts_used = QtWidgets.QLabel("Parts Cost")
-        self.parts_used.setFont(main_font)
-        self.block_layout.addWidget(self.parts_used, 7, 1, 1, 1)
+        #self.parts_used = QtWidgets.QLabel("Parts Cost")
+        #self.parts_used.setFont(main_font)
+        #self.block_layout.addWidget(self.parts_used, 7, 2, 1, 1)
 
-        self.external_costs = QtWidgets.QLabel("List Of External Costs")
-        self.external_costs.setFont(main_font)
-        self.block_layout.addWidget(self.external_costs, 8, 1, 1, 1)
+        #self.external_costs = QtWidgets.QLabel("List Of External Costs")
+        #self.external_costs.setFont(main_font)
+        #self.block_layout.addWidget(self.external_costs, 8, 1, 1, 1)
 
-        self.block_layout.addItem(self.vertical_spacer, 9, 0, 1, 4)
-        self.block_layout.addItem(self.vertical_spacer, 0, 2, 8, 1)
+        #self.block_layout.addItem(self.vertical_spacer, 9, 0, 1, 4)
+        #self.block_layout.addItem(self.vertical_spacer, 0, 2, 8, 1)
 
         self.block_widget.setLayout(self.block_layout)
-        self.grid_layout.addWidget(self.block_widget, 3, 0, 4, 1)
+        self.grid_layout.addWidget(self.block_widget, 1, 2, 1, 1)
 
         # Cylinder Head
-
-        self.head_label = QtWidgets.QLabel("Cylinderhead")
-        self.head_label.setFont(title_font)
-        self.head_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.grid_layout.addWidget(self.head_label, 1, 1, 1, 1)
-
         self.head_widget = QtWidgets.QWidget()
         self.head_layout = QtWidgets.QGridLayout()
         self.head_layout.setColumnMinimumWidth(0, 1)
 
-        self.procedures = QtWidgets.QLabel("Procedures")
-        self.procedures.setAlignment(QtCore.Qt.AlignLeft)
-        self.procedures.setFont(main_font)
-        self.head_layout.addWidget(self.procedures, 0, 0, 1, 2)
-
-        self.time = QtWidgets.QLabel("Time")
-        self.time.setFont(main_font)
-        self.head_layout.addWidget(self.time, 0, 3, 1, 1)
-
-        self.cost = QtWidgets.QLabel("Cost")
-        self.cost.setFont(main_font)
-        self.cost.setAlignment(QtCore.Qt.AlignCenter)
-        self.head_layout.addWidget(self.cost, 0, 4, 1, 1)
-
-        self.cylhead_checkbox = QtWidgets.QCheckBox()
-        self.head_layout.addWidget(self.cylhead_checkbox, 1, 0, 1, 1)
         self.cylhead_label = QtWidgets.QLabel("Cylinder Head")
         self.cylhead_label.setFont(main_font)
-        self.head_layout.addWidget(self.cylhead_label, 1, 1, 1, 1)
+        self.head_layout.addWidget(self.cylhead_label, 1, 0, 1, 1)
 
-        self.intcam_checkbox = QtWidgets.QCheckBox()
-        self.head_layout.addWidget(self.intcam_checkbox, 2, 0, 1, 1)
-        self.intcam_label = QtWidgets.QLabel("Intake Camshaft 1")
+        self.intcam_label = QtWidgets.QLabel("Intake Cam")
         self.intcam_label.setFont(main_font)
-        self.head_layout.addWidget(self.intcam_label, 2, 1, 1, 1)
+        self.head_layout.addWidget(self.intcam_label, 2, 0, 1, 1)
 
-        self.exhcam_checkbox = QtWidgets.QCheckBox()
-        self.head_layout.addWidget(self.exhcam_checkbox, 3, 0, 1, 1)
-        self.exhcam_label = QtWidgets.QLabel("Exhaust Camshaft 1")
+        self.exhcam_label = QtWidgets.QLabel("Exhaust Cam")
         self.exhcam_label.setFont(main_font)
-        self.head_layout.addWidget(self.exhcam_label, 3, 1, 1, 1)
+        self.head_layout.addWidget(self.exhcam_label, 3, 0, 1, 1)
 
-        self.head_layout.addItem(self.vertical_spacer, 9, 0, 1, 4)
-        self.head_layout.addItem(self.vertical_spacer, 0, 2, 8, 1)
+        #self.head_layout.addItem(self.vertical_spacer, 9, 0, 1, 4)
+        #self.head_layout.addItem(self.vertical_spacer, 0, 2, 8, 1)
+
         self.head_widget.setLayout(self.head_layout)
-        self.grid_layout.addWidget(self.head_widget, 3, 1, 4, 1)
+        self.grid_layout.addWidget(self.head_widget, 2, 2, 1, 1)
 
-        # Extras
-
-        self.extras_label = QtWidgets.QLabel("Extras")
-        self.extras_label.setFont(title_font)
-        self.extras_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.grid_layout.addWidget(self.extras_label, 1, 2, 1, 1)
+        self.grid_layout.addItem(self.spacer, 1, 1, 3, 1)
 
         self.grid_layout.addWidget(self.menu, 0, 0, 1, 3)
-        self.grid_layout.addItem(self.vertical_spacer, 3, 0, 1, 3)
+        #self.grid_layout.addItem(self.vertical_spacer, 3, 0, 1, 3)
 
         self.setLayout(self.grid_layout)
 
