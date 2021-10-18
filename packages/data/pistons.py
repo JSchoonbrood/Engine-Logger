2efe261def25861f4c5faf982be8634f10f9dc5e
JSchoonbrood <schoonbrood.jake@gmail.com>
2021-10-18 14:02:22 +0100

    Initial Layout Design

--- packages/data/pistons.py
+++ packages/data/pistons.py
@@ -20,4 +20,61 @@ class Widget(QtWidgets.QWidget):
         self.grid_layout.setContentsMargins(5, 5, 5, 5)
         self.grid_layout.setSpacing(5)
 
+        # Row 1
+        
+        self.make_label = QtWidgets.QLabel("Piston Make")
+        self.make_input = QtWidgets.QLineEdit()
+
+        # Row 2
+
+        self.diameter_label = QtWidgets.QLabel("Piston Diameter")
+        self.piston_diameter_input = QtWidgets.QLineEdit()
+
+        self.clearance_label = QtWidgets.QLabel("Piston Clearance")
+        self.clearance_input = QtWidgets.QLineEdit()
+
+        self.weight_label = QtWidgets.QLabel("Piston Weight WOut/ Pin")
+        self.weight_input = QtWidgets.QLineEdit()
+        
+        # Row 3
+
+        self.wristpin_weight_label = QtWidgets.QLabel("Wristpin Weight")
+        self.wristpin_weight_input = QtWidgets.QLineEdit()
+
+        self.wristpin_od_label = QtWidgets.QLabel("Wristpin Outer Diameter")
+        self.wristpin_od_input = QtWidgets.QLineEdit()
+
+        self.ring_table = QtWidgets.QTableWidget()
+        vertical_labels = ['Ring Type', 'Ring Thickness', 'Ring Gap']
+        hor_labels = ['Top Ring', 'Second Ring', 'Oil Ring']
+
+        self.ring_table.setRowCount(3)
+        self.ring_table.setVerticalHeaderLabels(vertical_labels)
+
+        self.ring_table.setColumnCount(3)
+        self.ring_table.setHorizontalHeaderLabels(hor_labels) 
+        
+        #self.grid_layout.addWidget(self.ring_table)
+
+        # Add Widgets To Layout
+
+        self.grid_layout.addWidget(self.make_label, 0, 0, 1, 1)
+        self.grid_layout.addWidget(self.make_input, 0, 1, 1, 2)
+
+        self.grid_layout.addWidget(self.diameter_label, 1, 0, 1, 1)
+        self.grid_layout.addWidget(self.piston_diameter_input, 2, 0, 1, 1)
+        self.grid_layout.addWidget(self.clearance_label, 1, 1, 1, 1)
+        self.grid_layout.addWidget(self.clearance_input, 2, 1, 1, 1)
+        self.grid_layout.addWidget(self.weight_label, 1, 2, 1, 1)
+        self.grid_layout.addWidget(self.weight_input, 2, 2, 1, 1)
+
+        self.grid_layout.addWidget(self.wristpin_weight_label, 4, 0, 1, 1)
+        self.grid_layout.addWidget(self.wristpin_weight_input, 5, 0, 1, 1)
+        self.grid_layout.addWidget(self.wristpin_od_label, 4, 1, 1, 1)
+        self.grid_layout.addWidget(self.wristpin_od_input, 5, 1, 1, 1)
+
+        self.grid_layout.addWidget(self.ring_table, 6, 0, 1, 3)
+
+        # Set Layouts
+
         self.setLayout(self.grid_layout)
