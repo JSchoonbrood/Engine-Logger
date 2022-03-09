46087692984c72e526c440aac805f0a1aba546ce
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-09 16:49:13 +0000

    Fixed table stretch

--- packages/data/pistons.py
+++ packages/data/pistons.py
@@ -1,15 +1,10 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 from packages.backend import connection
 
-title_font = QtGui.QFont()
-title_font.setPointSize(18)
-
-main_font = QtGui.QFont()
-main_font.setPointSize(17)
-
 class Widget(QtWidgets.QWidget):
     def __init__(self, directory, job_id, parent=None):
         super(Widget, self).__init__()
+        self.setObjectName("pistons")
         self.directory = str(directory)
         self.job_id = job_id
         self.constrct_ui()
@@ -21,12 +16,10 @@ class Widget(QtWidgets.QWidget):
         self.grid_layout.setSpacing(5)
 
         # Row 1
-        
         self.make_label = QtWidgets.QLabel("Piston Make")
         self.make_input = QtWidgets.QLineEdit()
 
         # Row 2
-
         self.diameter_label = QtWidgets.QLabel("Piston Diameter")
         self.piston_diameter_input = QtWidgets.QLineEdit()
 
@@ -37,7 +30,6 @@ class Widget(QtWidgets.QWidget):
         self.weight_input = QtWidgets.QLineEdit()
         
         # Row 3
-
         self.wristpin_weight_label = QtWidgets.QLabel("Wristpin Weight")
         self.wristpin_weight_input = QtWidgets.QLineEdit()
 
@@ -50,14 +42,15 @@ class Widget(QtWidgets.QWidget):
 
         self.ring_table.setRowCount(3)
         self.ring_table.setVerticalHeaderLabels(vertical_labels)
+        self.ring_table.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
         self.ring_table.setColumnCount(3)
         self.ring_table.setHorizontalHeaderLabels(hor_labels) 
-        
+        self.ring_table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         #self.grid_layout.addWidget(self.ring_table)
 
-        # Add Widgets To Layout
 
+        # Add Widgets To Layout
         self.grid_layout.addWidget(self.make_label, 0, 0, 1, 1)
         self.grid_layout.addWidget(self.make_input, 0, 1, 1, 2)
 
@@ -76,5 +69,4 @@ class Widget(QtWidgets.QWidget):
         self.grid_layout.addWidget(self.ring_table, 6, 0, 1, 3)
 
         # Set Layouts
-
         self.setLayout(self.grid_layout)
